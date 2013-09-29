922893019178be6d32770097eacacc18980c7d38
emmanuel negrete <emmanuel@emmanuelnegrete.net>
2013-09-29 14:29:47 -0500

    Cree una aplicacion lol

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MiApp::Application.config.secret_key_base = 'd19c6a93ce4a5d33dbe1e2c6914f955c4b363dd5c86536fe205dd0a9fcc1bdda08ab6d2b9949d06af576261065ca390dbc1ea561d37e1f4f81bac6bc49d2defe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MiApp::Application.config.session_store :cookie_store, key: '_miApp_session'
